28515c2bea7f1f80bb11a50bce69e2284aff2eef
pyson96 <pyson96@gmail.com>
2025-04-08 11:01:25 +0900

    ADD : zigzag conversation
    list 에 방문 표시하며 접근

---
+++ zigzagConversation/list.py
@@ -0,0 +1,33 @@
+class Solution:
+    visitArr : list
+    s : str
+    answer : str 
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 : return s
+        self.visitArr = [False] * len(s)
+        self.s = s
+        self.answer = ""
+        stride = numRows - 1 
+        while stride >= 0 :
+            i = numRows - stride - 1
+            while i < len(self.s):
+                i = self.visitNextElement(i)
+                i = i + stride * 2       
+            stride -= 1
+        return self.answer
+    def visitNextElement(self,idx):
+        while idx < len(self.s) :
+            self.expandAnswer(idx)
+            if idx + 1 < len(self.s) and self.visitArr[idx+1] == True:
+                idx += 1
+                while idx < len(self.s) and self.visitArr[idx] == True :
+                    idx += 1
+            else :
+                return idx
+        return idx
+    def expandAnswer(self, idx) :
+        self.visitArr[idx] = True
+        self.answer = self.answer + self.s[idx]
+        
+s = Solution()
+s.convert("AB",2)
