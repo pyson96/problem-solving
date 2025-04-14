dbe0524fc6e622021be2a339576ddf9408c35dad
pyson96 <pyson96@gmail.com>
2025-04-14 22:01:59 +0900

    UPDATE : string 알고리즘
    반절씩 비교하는 것이아니라
    전체 문자열이 거꾸로 해도 똑같은 palindrome 성질을 이용하여 코드를 더 간편하게 바꿈

--- palindromeNumber/string.py
+++ palindromeNumber/string.py
@@ -1,8 +1,5 @@
-import math
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        left = len(str(x)) // 2 
-        right = math.ceil((len(str(x)) / 2 ))
-        return str(x)[:left:] == str(x)[-1:right-1:-1]
+        return str(x) == str(x)[::-1]
 s = Solution()
 s.isPalindrome(1001)
